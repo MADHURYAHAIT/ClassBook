f317851ce2b42ca7d8cf53cb414412a01590daab
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2023-11-14 04:39:32 +0530

    profile complete

--- react/myapp/src/component/Basics/Profile.js
+++ react/myapp/src/component/Basics/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import{BiSolidUserPin} from 'react-icons/bi'
 import NavBarr from './NavBarr'
 import Footer from './Footer'
@@ -10,8 +10,11 @@ const clientId="36910271547-g4gf1pe3a3ln92sfmj6n293s5k3vfnrp.apps.googleusercont
 
 
 const Profile = (Prp) => {
-    var url=Prp.profData.imageUrl;
-    const[Img,setImg]=url;
+ 
+        const url=Prp.profData.imageUrl;
+        const[Img,seturl]=useState(url);
+   
+    
   return (
     <div className='Profile'>
        
@@ -23,14 +26,17 @@ const Profile = (Prp) => {
             </div>
         </div>
 
-        <div class="container">
-            <div class="front">
+        <div class="profcontainer">
+            <div class="front-profile">
            
             <div class="section2">
-                <h1 className='book'>{}</h1>
-                <img href={Img}/>
-                User Name : {Prp.profData.name}<br/>
-                email Id : {Prp.profData.email}
+                <h1 className='book'>{Prp.profData.givenName}</h1>
+                <h5 style={{color:'white'}}>Profile</h5><br/>
+                {/* <img href={Img}/> */}
+                <img src={url} alt={Prp.profData.name}/>
+                <hr/>
+                 User Name :  <i style={{color:'white'}}>{Prp.profData.name}</i><br/>
+                email Id : <i style={{color:'white'}}>{Prp.profData.email}</i>
 
                     <hr/>
                     <GoogleLogout/>
