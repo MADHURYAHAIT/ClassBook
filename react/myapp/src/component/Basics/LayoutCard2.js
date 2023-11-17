9a11342cdeac7e65dec236f9a71a00b1faf1355e
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2023-11-18 01:54:32 +0530

    Students Tab

--- react/myapp/src/component/Basics/LayoutCard2.js
+++ react/myapp/src/component/Basics/LayoutCard2.js
@@ -8,37 +8,58 @@ const LayoutCard= ({studData}) => {
   return (
     <>
 
-    <section className='main-card--cointainer ' >
+    <section className='main-card--cointainer2 ' >
     {studData.map((curElem)=>{
         return(
         <>
-            <div className='card-container' key={curElem}>
-            <div className='card'>
+            <div className='card-container2' key={curElem}>
+             
                 <div className='card-body'>
-                <div class="imageholder">
-                  <span className='card-author subtle'>Department of Ai & Ds</span><br/>
-                      <div class='circularframe'>
-                        <img src={curElem.image} alt="images" className='card-media'/>
+
+
+                <div class="col" ontouchstart="this.classList.toggle('hover');">
+                  <div class="container">
+
+                    <div class="front" style={{backgroundImage: `url(${curElem.image})`}}>
+                      <div class="inner">
+                        <p>{curElem.Name}</p>
+                        <span>Reg No. : {curElem.RegNo}</span>
                       </div>
                     </div>
-                  <hr/>
-                  <div className='txt-mid'>
-                    <h2 className='card-title'>{curElem.Name}</h2>
+                    
+                    <div class="back">
+                      <div class="inner">
+                        <p>
+                        <span className='card-description subtle'>       
+                            Dob : <i style={{color:'white'}}>{curElem.DOB}</i> <br/>
+                            Category : <i style={{color:'white'}}>{curElem.Category}</i> <br/>
+                            Gender : <i style={{color:'white'}}>{curElem.sex}</i> <br/>
+                            Father Name : <i style={{color:'white'}}>{curElem.Father}</i> <br/>
+                            CPGA : <i style={{color:'white'}}>{curElem.CGPA}</i> <br/>
+                            Contact : <i style={{color:'white'}}>{curElem.Contact} </i> <br/>
+                        </span>
+                        </p>
+                      </div>
+                    </div>
+                    
+                  </div>
                 </div>
-                  <span className='card-description subtle'>       
-                    Reg No : <i style={{color:'white'}}>{curElem.RegNo}</i> <br/>
-                    Dob : <i style={{color:'white'}}>{curElem.DOB}</i> <br/>
-                    Category : <i style={{color:'white'}}>{curElem.Category}</i> <br/>
-                    Gender : <i style={{color:'white'}}>{curElem.sex}</i> <br/>
-                    Father Name : <i style={{color:'white'}}>{curElem.Father}</i> <br/>
-                    CPGA : <i style={{color:'white'}}>{curElem.CGPA}</i> <br/>
-                    Contact : <i style={{color:'white'}}>{curElem.Contact} </i> <br/>
-                  </span>
-                  {/* <div className='card-read'> Photo</div> */}
-    
-                </div>
+
+
+
+
+
+
+
+              
             </div>
-        </div> 
+
+
+
+
+                 
+            </div>
+      
        
     </>
         )
