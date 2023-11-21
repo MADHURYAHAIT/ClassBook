e15e533c3f0bbe87f1231e1d2f9700d6cb717865
MADHURYAHAIT <madhurya_202100516@smit.smu.edu.in>
2023-11-21 23:23:25 +0530

    .

--- react/myapp/src/component/Basics/home.js
+++ react/myapp/src/component/Basics/home.js
@@ -11,9 +11,8 @@ import {FcGoogle} from 'react-icons/fc'
 export const logo ='./logo/1.png'
 export const logo2 ='./logo/google.png'
 
-
 const clientId="36910271547-g4gf1pe3a3ln92sfmj6n293s5k3vfnrp.apps.googleusercontent.com";
-const Home = () =>  {
+const Home = ()  =>  {
   
   const[UserData,setData]=useState('f')
   const[flag,setFlag]=useState(true);
