43925111731265695f115970f8dd2b1b8762ebd0
Madhurya Hait <101663118+MADHURYAHAIT@users.noreply.github.com>
2023-12-18 22:21:25 +0530

    Update index.js

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 // import './Style.scss'
-import App from './App';
+import App from './App' ;
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
